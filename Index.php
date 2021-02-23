a57314efaf55acf90d9bd2aea860d7330c6fbf42
Héctor González Pereda <hgonza02@ucm.es>
2021-02-23 23:37:28 +0100

    add login-container

---
+++ Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Sign or login </title>
+<link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<div class='container'>
+    <div class='login-container'>
+        <h1>Welcome</h1>
+        <h2>Para acceder a la página web, introduzca sus credenciales</h2>
+        <a href='#'>Sing up</a>
+        <a href='#'>Login</a>
+    </div>
+</div>
+</body>
+</html>
