c55e82b5581554974867894882da19636e4f58bc
Héctor González Pereda <hgonza02@ucm.es>
2021-02-24 18:23:38 +0100

    add login.php css

--- login.php
+++ login.php
@@ -3,15 +3,24 @@
 <head>
 <title> Sign-up  </title>
 <link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/login.css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
 </head>
 <body>
 <div class='container'>
     <div class='login-container'>
-    <div class='login-title'>
+        <div class='login-title'>
         <h1>Login</h1>
         </div>
+            <div class='login-form'>
+            <form action='login.php' method='post'>
+            <input type='text' name='nick' placeholder='Enter your nick'>
+            <input type='text' name='email' placeholder='Enter your email'>
+            <input type='password' name='password' placeholder='Enter your password'>
+            <input type='submit' value='send'>
+            </form>
+            </div>  
     </div>
 </div>
 </body>
